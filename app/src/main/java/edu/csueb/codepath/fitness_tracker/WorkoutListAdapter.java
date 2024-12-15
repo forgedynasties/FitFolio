e5cd493161a0c5ddb9437e6f5eb2c14424e7af0e
forgedynasties <112465473+forgedynasties@users.noreply.github.com>
2024-12-15 10:55:36 +0500

    Moved all binding logic to the bind() method for better separation of concerns.
    Dynamically sets the CheckBox state and handles click events within bind().
    
    Used notifyDataSetChanged() sparingly and offered finer-grained updates like notifyItemChanged() for individual items.
    
    Improved the clarity of log messages to make debugging easier.
    
    Ensured that the workouts and checkedWorkouts lists are initialized safely to prevent NullPointerException.
    
    Added a setter for the OnClickListener to allow flexible callback registration.

--- app/src/main/java/edu/csueb/codepath/fitness_tracker/WorkoutListAdapter.java
+++ app/src/main/java/edu/csueb/codepath/fitness_tracker/WorkoutListAdapter.java
@@ -1,78 +1,53 @@
 package edu.csueb.codepath.fitness_tracker;
 
-import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.CheckBox;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import edu.csueb.codepath.fitness_tracker.fragments.TrackFragment;
 
 public class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.ViewHolder> {
 
-    public interface OnClickListener{
+    public interface OnClickListener {
         void onItemClicked(int position);
     }
 
-    private List<String> workouts;
-    private List<String> checked;
-    private TrackFragment activity;
+    private List<String> workouts = new ArrayList<>();
+    private final List<String> checkedWorkouts = new ArrayList<>();
+    private final TrackFragment trackFragment;
+    private OnClickListener clickListener;
 
-    OnClickListener clickListener;
-
-    public WorkoutListAdapter(List<String> list, TrackFragment trackFragment){ //, OnClickListener clicklistener
-        this.workouts = list;
-        this.activity = trackFragment;
-        checked = new ArrayList<>();
-        //this.clickListener = clicklistener;
+    // Constructor
+    public WorkoutListAdapter(List<String> workoutList, TrackFragment trackFragment) {
+        this.workouts = workoutList != null ? workoutList : new ArrayList<>();
+        this.trackFragment = trackFragment;
     }
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    // Setter for click listener
+    public void setOnClickListener(OnClickListener listener) {
+        this.clickListener = listener;
+    }
 
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.workout_task_layout, parent, false);
-
         return new ViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        //RecyclerView cv;
-        //cv = holder.cv.findViewById(R.id.rvWorkout);
-        //cv.setLayoutAnimation(AnimationUtils.loadLayoutAnimation(holder.cv.getContext(), R.anim.layout_task_list_anim));
-
-
-        String item = workouts.get(position);
-        holder.task.setText(item);
-        holder.task.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(holder.task.isChecked()){
-                    Log.i("WorkoutListAdapter", holder.task.getText().toString());
-                    checked.add(holder.task.getText().toString());
-                } else if(!holder.task.isChecked()){
-                    //Log.i("WorkoutListAdapter", holder.task.getText().toString() + " Is unchecked");
-                    if(checked.contains(holder.task.getText().toString())){
-                        Log.i("WorkoutListAdapter", holder.task.getText().toString() + " Is unchecked");
-                        checked.remove(holder.task.getText().toString());
-                    }
-                }
-
-            }
-        });
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String workout = workouts.get(position);
+        holder.bind(workout, position);
     }
 
     @Override
@@ -80,36 +55,46 @@ public class WorkoutListAdapter extends RecyclerView.Adapter<WorkoutListAdapter.
         return workouts.size();
     }
 
-    public void setWorkouts(List<String> workoutList){
-        this.workouts = workoutList;
-        this.notifyDataSetChanged();
+    // Update workout list
+    public void setWorkouts(List<String> workoutList) {
+        this.workouts = workoutList != null ? workoutList : new ArrayList<>();
+        notifyDataSetChanged();
     }
 
-
-    public List<String> getChecked(){
-        Log.i("WorkoutListAdapter", Arrays.toString(checked.toArray()));
-        return checked;
+    // Get checked workouts
+    public List<String> getCheckedWorkouts() {
+        Log.i("WorkoutListAdapter", "Checked workouts: " + checkedWorkouts);
+        return new ArrayList<>(checkedWorkouts);
     }
 
+    // ViewHolder class
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private final CheckBox checkBox;
 
-    class ViewHolder extends RecyclerView.ViewHolder{
-        public CheckBox task;
-        //public RecyclerView cv;
-        private Context context;
-
-        public ViewHolder(View view){
-            super(view);
-            task = (CheckBox) view.findViewById(R.id.checkbox);
-            //cv = view.findViewById(R.id.rvWorkout);
+        public ViewHolder(View itemView) {
+            super(itemView);
+            checkBox = itemView.findViewById(R.id.checkbox);
         }
 
-        public void bind(String item) {
-            task.setText(item);
-            task.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    clickListener.onItemClicked(getAdapterPosition());
+        public void bind(String workout, int position) {
+            checkBox.setText(workout);
+            checkBox.setChecked(checkedWorkouts.contains(workout));
 
+            // Handle checkbox click
+            checkBox.setOnClickListener(v -> {
+                if (checkBox.isChecked()) {
+                    if (!checkedWorkouts.contains(workout)) {
+                        checkedWorkouts.add(workout);
+                        Log.i("WorkoutListAdapter", "Checked: " + workout);
+                    }
+                } else {
+                    checkedWorkouts.remove(workout);
+                    Log.i("WorkoutListAdapter", "Unchecked: " + workout);
+                }
+
+                // Notify listener if set
+                if (clickListener != null) {
+                    clickListener.onItemClicked(position);
                 }
             });
         }
